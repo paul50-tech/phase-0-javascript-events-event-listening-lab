bd647d6bc3483ec9c8e097b64f0761d83477c064
Paul Jubilee Owuoth <pauljubilee50@gmail.com>
2024-10-02 20:07:52 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+
 function addingEventListener() {
+  const input = document.getElementById('button');
+  
+  if (input) {
+      input.addEventListener('click', function() {
+          alert('I was clicked!');
+      });
+  } else {
+      console.error("Button with id 'button' not found.");
+  }
 }
+
+addingEventListener();
